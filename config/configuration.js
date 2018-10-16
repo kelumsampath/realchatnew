6c2a7330158a8ff3a41a66882becce966d1d72d1
Kelum Sampath Edirisinghe <edirisingheemks@gmail.com>
2018-10-16 21:08:24 +0530

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,11 +1,6 @@
 module.exports = {
     "database" : "mongodb://real12345:real12345@ds131963.mlab.com:31963/real",
 
-    "cloud_name":"defah2zln",
-    "api_key":"227588295695171",
-    "api_secrete":"H3qw5KLfuUeLAi6dYvhqXFKu3I8",
-    "sendgrid_apikey":"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"
-
 };
 
 //
